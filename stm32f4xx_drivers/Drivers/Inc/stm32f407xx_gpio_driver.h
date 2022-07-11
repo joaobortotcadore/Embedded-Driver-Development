ba2ffb050d6b291ba9560ea30a740be500cc25c4
joaobortotcadore <joaobortotcadore@gmail.com>
2022-07-10 22:35:36 -0300

    Section 23 - Lesson 90: Chore: Add API prototypes

--- stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio_driver.h
+++ stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio_driver.h
@@ -28,4 +28,36 @@ typedef  struct
 	GPIO_PinCOnfig_t 	GPIO_PinConfig;			/* This holds GPIO pin configuration settings */
 }GPIO_Handle_t;
 
+
+/**************************************
+ * APIs supported by this driver
+ * ************************************/
+/*
+ * Peripheral Clock setup
+ */
+void GPIO_PeriClockControl(void);
+
+/*
+ * Init and De-init
+ */
+void GPIO_Init(void);
+void GPIO_DeInit(void);
+
+/*
+ * Data read/write
+ */
+void GPIO_ReadFromInputPin(void);
+void GPIO_ReadFromInputPort(void);
+void GPIO_WriteToOutputPin(void);
+void GPIO_WriteToOutputPort(void);
+void GPIO_ToggleOutputPin(void);
+
+/*
+ * IRQ Configuration and ISR handling | IRQ - Interrupt ReQuest, ISR - Interrupt Status Register
+ */
+void GPIO_IRQConfig(void); //to enable it, to set priority, etc
+void GPIO_IRQHandling(void); //when an interrupt occurs, this function is responsible to process
+
+
+
 #endif /* INC_STM32F407XX_GPIO_DRIVER_H_ */
