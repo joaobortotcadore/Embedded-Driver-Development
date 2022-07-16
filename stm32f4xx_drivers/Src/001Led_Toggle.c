745d57cb8c928e0e64e3045d8cc5393278a03960
joaobortotcadore <joaobortotcadore@gmail.com>
2022-07-15 23:42:47 -0300

    Section 27 - Lesson:102: LED toggling with PUSH PULL configuration

---
+++ stm32f4xx_drivers/Src/001Led_Toggle.c
@@ -0,0 +1,16 @@
+/*
+ * 001Led_Toggle.c
+ *
+ *  Created on: 15 de jul de 2022
+ *      Author: joaobortotcadore
+ */
+
+#include "stm32f407xx.h"
+
+int main(void)
+{
+	GPIO_Handle_t GpioLed;
+
+	return 0;
+}
+
