35560f114fddd9fcd9dce326b7a48774e760a012
joaobortotcadore <joaobortotcadore@gmail.com>
2022-07-19 00:18:43 -0300

    Section 27 - Lesson:103: Exercise: LED toggling with Open Drain configuration

--- stm32f4xx_drivers/Src/001Led_Toggle.c
+++ stm32f4xx_drivers/Src/001Led_Toggle.c
@@ -10,9 +10,10 @@
 /* simple delay by software */
 void delay(void)
 {
-    for(uint32_t i =0; i< 500000; i++);
+    for(uint32_t i =0; i< 500000/2; i++);
 }
-int main(void)
+
+void blinkGreenLedPP(void) /* PushPull configuration */
 {
 	GPIO_Handle_t GpioLed;
 	GpioLed.pGPIOx = GPIOD;
@@ -27,9 +28,34 @@ int main(void)
 
 	while(1)
 	{
-	    GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
-	    delay();
+		GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+		delay();
 	}
+}
+
+void blinkGreenLedOD(void) /* Open Drain configuration */
+{
+	GPIO_Handle_t GpioLed;
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_OD;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_PU; /* PullUp resistor = 40K ohms, the brightness gets weak */
+	/* green led is connected to 510 ohm resistor, connect PD12 to 1K or less resistor in protoboard and 5V, and use this function */
+	GPIO_PeriClockControl(GPIOD,ENABLE);
+	GPIO_Init(&GpioLed);
+
+	while(1)
+	{
+		GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+		delay();
+	}
+}
+
+int main(void)
+{
+	blinkGreenLedOD();
 	return 0;
 }
 
