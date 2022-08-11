43ad828eef790c0e5c1418382390fd7765db1263
joaobortotcadore <joaobortotcadore@gmail.com>
2022-08-11 13:34:51 -0300

    Chore: fix SPI error associated with bit SSM in SPI_CR1 register

--- stm32f4xx_drivers/Drivers/Src/stm32f407xx_spi_driver.c
+++ stm32f4xx_drivers/Drivers/Src/stm32f407xx_spi_driver.c
@@ -25,11 +25,11 @@ void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnOrDi)
         {
             SPI1_PCLK_EN();
         }
-        if(pSPIx == SPI2)
+        else if(pSPIx == SPI2)
         {
             SPI3_PCLK_EN();
         }
-        if(pSPIx == SPI3)
+        else if(pSPIx == SPI3)
         {
             SPI3_PCLK_EN();
         }
@@ -40,18 +40,18 @@ void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnOrDi)
     }
     else
     {
-        if(pSPIx == SPI1)
-        {
-            SPI1_PCLK_DI();
-        }
-        if(pSPIx == SPI2)
-        {
-            SPI3_PCLK_DI();
-        }
-        if(pSPIx == SPI3)
-        {
-            SPI3_PCLK_DI();
-        }
+        // if(pSPIx == SPI1)
+        // {
+        //     SPI1_PCLK_DI();
+        // }
+        // if(pSPIx == SPI2)
+        // {
+        //     SPI3_PCLK_DI();
+        // }
+        // if(pSPIx == SPI3)
+        // {
+        //     SPI3_PCLK_DI();
+        // }
 //        if(pSPIx == SPI4)
 //        {
 //            SPI4_PCLK_DI();
@@ -77,25 +77,25 @@ void SPI_Init(SPI_Handle_t *pSPIHandle)
 	SPI_PeriClockControl(pSPIHandle->pSPIx, ENABLE);
 
 	//1. configure the device mode
-	tempreg |= pSPIHandle->SPIConfig.SPI_DeviceMode << 2;
+	tempreg |= pSPIHandle->SPIConfig.SPI_DeviceMode << SPI_CR1_MSTR;
 
 	//2. Configure the bus config
 	if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_FD)
 	{
 		//bidi mode should be cleared
-		tempreg &= ~(1<<15);
+		tempreg &= ~(1 << SPI_CR1_BIDIMODE);
 
 	}else if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_HD)
 	{
 		//bidi mode should be set
-		tempreg |= (1<<15);
+		tempreg |= (1 << SPI_CR1_BIDIMODE);
 
 	}else if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_SIMPLEX_RXONLY)
 	{
 		//bidi mode should be cleared
-		tempreg &= ~(1<<15);
+		tempreg &= ~(1 << SPI_CR1_BIDIMODE);
 		//RXONLY bit must be set
-		tempreg |= (1<<10);
+		tempreg |= (1 << SPI_CR1_RXONLY);
 
 	}
 
@@ -103,13 +103,15 @@ void SPI_Init(SPI_Handle_t *pSPIHandle)
 	tempreg |= pSPIHandle->SPIConfig.SPI_SclkSpeed << SPI_CR1_BR;
 
 	//4. Configure the DFF
-	tempreg |= pSPIHandle->SPIConfig.SPI_DFF << 11;
+	tempreg |= pSPIHandle->SPIConfig.SPI_DFF << SPI_CR1_DFF;
 
 	//5. Configure the CPOL
 	tempreg |= pSPIHandle->SPIConfig.SPI_CPOL << SPI_CR1_CPOL;
 
 	//6. Configure the CPHA
 	tempreg |= pSPIHandle->SPIConfig.SPI_CPHA << SPI_CR1_CPHA;
+
+	tempreg |= pSPIHandle->SPIConfig.SPI_SSM << SPI_CR1_SSM;
 
 	pSPIHandle->pSPIx->CR1 = tempreg;
 }
