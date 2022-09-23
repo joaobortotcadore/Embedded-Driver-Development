493ecf24a95019375f5573772611d0bbe9ef3ed0
joaocadore <joao.cadore@inobram.com.br>
2022-09-23 16:52:35 -0300

    Section 59 - Lesson 204-207: I2C master receive data
    Chore: Implementation of I2C master receive data API

--- stm32f4xx_drivers/Drivers/Inc/stm32f407xx_i2c_driver.h
+++ stm32f4xx_drivers/Drivers/Inc/stm32f407xx_i2c_driver.h
@@ -91,7 +91,7 @@ void I2C_DeInit(I2C_RegDef_t *pI2Cx);
  * Data Send and Receive
  */
 void I2C_MasterSendData(I2C_Handle_t *pI2CHandle,uint8_t *pTxbuffer, uint32_t Len, uint8_t SlaveAddr,uint8_t Sr);
-//void I2C_MasterReceiveData(I2C_Handle_t *pI2CHandle,uint8_t *pRxBuffer, uint8_t Len, uint8_t SlaveAddr,uint8_t Sr);
+void I2C_MasterReceiveData(I2C_Handle_t *pI2CHandle,uint8_t *pRxBuffer, uint8_t Len, uint8_t SlaveAddr,uint8_t Sr);
 //uint8_t I2C_MasterSendDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pTxbuffer, uint32_t Len, uint8_t SlaveAddr,uint8_t Sr);
 //uint8_t I2C_MasterReceiveDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pRxBuffer, uint8_t Len, uint8_t SlaveAddr,uint8_t Sr);
 //
@@ -116,7 +116,7 @@ void I2C_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
  */
 void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
 uint8_t I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx , uint32_t FlagName);
-//void I2C_ManageAcking(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);
+void I2C_ManageAcking(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);
 //void I2C_GenerateStopCondition(I2C_RegDef_t *pI2Cx);
 //
 //void I2C_SlaveEnableDisableCallbackEvents(I2C_RegDef_t *pI2Cx,uint8_t EnorDi);
