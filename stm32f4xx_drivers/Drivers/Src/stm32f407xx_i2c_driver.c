f292ffc04164cc595932d77dcf28ec2b8d215743
joaocadore <joao.cadore@inobram.com.br>
2022-09-23 17:48:09 -0300

    Section 61 - Lesson 216: I2C adding interrupt related macros and interrupt APIs
    Chore: Just the prototypes

--- stm32f4xx_drivers/Drivers/Src/stm32f407xx_i2c_driver.c
+++ stm32f4xx_drivers/Drivers/Src/stm32f407xx_i2c_driver.c
@@ -285,6 +285,21 @@ void I2C_MasterReceiveData(I2C_Handle_t *pI2CHandle,uint8_t *pRxBuffer, uint8_t 
 
 }
 
+
+uint8_t I2C_MasterSendDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pTxbuffer, uint32_t Len, uint8_t SlaveAddr,uint8_t Sr)
+{
+
+}
+
+uint8_t I2C_MasterReceiveDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pRxBuffer, uint8_t Len, uint8_t SlaveAddr,uint8_t Sr)
+{
+
+}
+
+void I2C_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
+
+void I2C_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
+
 void I2C_ManageAcking(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
 {
     if(EnorDi == I2C_ACK_ENABLE)
